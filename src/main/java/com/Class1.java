dea2cfd359c10427852b1e6b163b27a688b65fea
Vladimir <vladimir.kaliadchyk@gmail.com>
2019-03-04 11:51:15 +0300

    wfsfdfg

--- src/main/java/com/Class1.java
+++ src/main/java/com/Class1.java
@@ -1,4 +1,8 @@
 package com;
 
 public class Class1 {
+    public static void main(String[] args) {
+        Class2 class2 = new Class2();
+        class2.print();
+    }
 }
---
+++ src/main/java/com/Class2.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Class2 {
+    public void print() {
+        System.out.println("Hello world!");
+    }
+}
